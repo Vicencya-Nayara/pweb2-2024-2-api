988214f6e3fc1b4326802a3d80360578ff5eaf12
Vicencya Nayara Diniz <115300537+Vicencya-Nayara@users.noreply.github.com>
2025-03-22 15:34:30 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,49 +1,51 @@
-'use strict';
-import { Model}  from 'sequelize';
-import bcrypt from 'bcrypt';
+import { Model } from "sequelize";
+import bcrypt from "bcrypt";
 
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 
 export default (sequelize) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Viagem, {
+        foreignKey: "userId",
+      });
+      User.belongsToMany(models.Viagem, {
+        through: "UserViagem", // Nome da tabela intermediária
+        foreignKey: "userId",
+        otherKey: "viagemId",
+        as: "viagems", // Alias para acessar as viagens do usuário
+      });
     }
   }
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.ENUM('admin', 'user'),
-      defaultValue: 'user' 
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+      },
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-    hooks: {
-      beforeCreate: async (user) => {
-        if(user.password){
-          const salt = await bcrypt.genSalt(10);
-          user.password = await bcrypt.hash(user.password, salt);
-        }   
-      }
-    }
-  });
+  );
+
   return User;
 };
